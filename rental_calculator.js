5f698e87cf5d9dd571aaebaf17f31d40d840a507
songguoqiang <songguoq@gmail.com>
2018-02-08 20:40:04 +0800

    [Gordon] refactoring: replace temp with query (to prepare for extract method)

--- rental_calculator.js
+++ rental_calculator.js
@@ -1,13 +1,16 @@
+function getMovie(movies, rental) {
+  return movies[rental.movieID];
+}
+
 export function statement(customer, movies) {
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
   for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
     let thisAmount = 0;
 
     // determine amount for each movie
-    switch (movie.code) {
+    switch (getMovie(movies, r).code) {
       case "regular":
         thisAmount = 2;
         if (r.days > 2) {
@@ -24,16 +27,17 @@ export function statement(customer, movies) {
         }
         break;
       default:
-        throw new Error("Invalid move type:" + movie.code);
+        throw new Error("Invalid move type:" + getMovie(movies, r).code);
     }
 
     //add frequent renter points
     frequentRenterPoints++;
     // add bonus for a two day new release rental
-    if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
+    if (getMovie(movies, r).code === "new" && r.days > 2)
+      frequentRenterPoints++;
 
     //print figures for this rental
-    result += `\t${movie.title}\t${thisAmount}\n`;
+    result += `\t${getMovie(movies, r).title}\t${thisAmount}\n`;
     totalAmount += thisAmount;
   }
   // add footer lines
