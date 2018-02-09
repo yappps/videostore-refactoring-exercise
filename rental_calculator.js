3ec6b2a623cdcd14532c40c376829ab1db0929fb
songguoqiang <songguoq@gmail.com>
2018-02-09 21:43:51 +0800

    [Gordon] refactoring: extract method

--- rental_calculator.js
+++ rental_calculator.js
@@ -1,7 +1,33 @@
 import Customer from "./customer";
 import Rental from "./rental";
 
-export function statement(customer, movies) {
+function getBillForRental(r) {
+  let bill = 0;
+
+  // determine amount for each movie
+  switch (r.movie.code) {
+    case "regular":
+      bill = 2;
+      if (r.days > 2) {
+        bill += (r.days - 2) * 1.5;
+      }
+      break;
+    case "new":
+      bill = r.days * 3;
+      break;
+    case "children":
+      bill = 1.5;
+      if (r.days > 3) {
+        bill += (r.days - 3) * 1.5;
+      }
+      break;
+    default:
+      throw new Error("Invalid move type:" + r.movie.code);
+  }
+  return bill;
+}
+
+function statement(customer, movies) {
   const c = new Customer({ name: customer.name });
   const rentals = customer.rentals.map(
     rental => new Rental({ movie: movies[rental.movieID], days: rental.days })
@@ -9,28 +35,7 @@ export function statement(customer, movies) {
   let totalAmount = 0;
   let result = `Rental Record for ${c.name}\n`;
   for (let r of rentals) {
-    let thisAmount = 0;
-
-    // determine amount for each movie
-    switch (r.movie.code) {
-      case "regular":
-        thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
-        }
-        break;
-      case "new":
-        thisAmount = r.days * 3;
-        break;
-      case "children":
-        thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
-        }
-        break;
-      default:
-        throw new Error("Invalid move type:" + r.movie.code);
-    }
+    let thisAmount = getBillForRental(r);
 
     //add frequent renter points
     c.incrementRentalPoints();
@@ -47,3 +52,5 @@ export function statement(customer, movies) {
 
   return result;
 }
+
+export { statement };
