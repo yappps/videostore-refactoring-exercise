1617bc08d3a347a8c90a23885670c848f83302c7
yyx <yuan_xin96@hotmail.com>
2018-03-10 14:55:25 +0800

    changing var name :  r to rental

--- src/rental_calculator.js
+++ src/rental_calculator.js
@@ -21,24 +21,24 @@ module.exports = function statement(customerRecord, movies) {
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
-  for (let r of rentals) {
-    let movie = movies[r.movie.id];
+  for (let rental of rentals) {
+    let movie = movies[rental.movie.id];
     let thisAmount = 0;
     // determine amount for each movie
     switch (movie.code) {
       case "regular":
         thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
+        if (rental.days > 2) {
+          thisAmount += (rental.days - 2) * 1.5;
         }
         break;
       case "new":
-        thisAmount = r.days * 3;
+        thisAmount = rental.days * 3;
         break;
       case "children":
         thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
+        if (rental.days > 3) {
+          thisAmount += (rental.days - 3) * 1.5;
         }
         break;
       default:
@@ -47,7 +47,7 @@ module.exports = function statement(customerRecord, movies) {
     //add frequent renter points
     frequentRenterPoints++;
     // add bonus for a two day new release rental
-    if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
+    if (movie.code === "new" && rental.days > 2) frequentRenterPoints++;
     //print figures for this rental
     result += `\t${movie.title}\t${thisAmount}\n`;
     totalAmount += thisAmount;
